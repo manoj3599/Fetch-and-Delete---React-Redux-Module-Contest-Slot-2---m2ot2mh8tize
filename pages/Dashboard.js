9c5554d0fe7b1ccd6ad51f42e574bd8f10dcaf30
manoj nayak <115462742+manoj3599@users.noreply.github.com>
2023-12-17 18:32:09 +0530

    Update Dashboard.js

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,15 +1,34 @@
-import React, { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "./_app";
-import DataItem from "./DataItem";
+import DataItem from "./dataItem";
 
-export default function Dashboard() {
-  const { data, deleteRow } = useContext(AuthContext);
+function Dashboard() {
+  const { data, setData, msg, setMsg } = useContext(AuthContext);
+
+  const handleDelete = (id) => {
+    // Check if a message is currently being displayed
+    if (!msg) {
+      // Filter out the data with the given id
+      const updatedData = data.filter((item) => item.id !== id);
+      // Update the data
+      setData(updatedData);
+      // Display the "Data Deleted" message for 3 seconds
+      setMsg("Data Deleted");
+      setTimeout(() => {
+        setMsg("");
+      }, 3000);
+    }
+  };
 
   return (
-    <div id="dashboard">
+    <div id="dashboardPage">
+      <div id="message">{msg}</div>
+
       {data.map((item) => (
-        <DataItem key={item.id} data={item} deleteRow={deleteRow} />
+        <DataItem key={item.id} {...item} onDelete={handleDelete} />
       ))}
     </div>
   );
 }
+
+export default Dashboard;
