d78b695a2af548536959236f5bee82f41b3a5e6c
manoj nayak <115462742+manoj3599@users.noreply.github.com>
2023-12-17 18:32:40 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,37 +1,31 @@
 import "@/styles/globals.css";
-import React, { useEffect, useState } from "react";
-import Dashboard from "./Dashboard";
-
+import React, { useState, useEffect } from "react";
 export const AuthContext = React.createContext();
 
 export default function App({ Component, pageProps }) {
   const [data, setData] = useState([]);
   const [msg, setMsg] = useState("");
 
-  const API = "https://content.newtonschool.co/v1/pr/63b70222af4f30335b4b3b9a/buses";
-
   useEffect(() => {
-    // Fetch data when the component mounts
-    fetch(API)
-      .then((response) => response.json())
-      .then((data) => setData(data));
-  }, []);
+    // Fetch data from the API
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://content.newtonschool.co/v1/pr/63b70222af4f30335b4b3b9a/buses");
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-  const deleteRow = (id) => {
-    // Filter out the row with the specified id
-    const newData = data.filter((item) => item.id !== id);
-    setData(newData);
-    setMsg("Data Deleted");
-
-    // Reset the message after 3 seconds
-    setTimeout(() => {
-      setMsg("");
-    }, 3000);
-  };
+    fetchData();
+  }, []); // Run this effect only once on component mount
 
   const contextValue = {
     data,
-    deleteRow,
+    setData,
+    msg,
+    setMsg,
   };
 
   return (
