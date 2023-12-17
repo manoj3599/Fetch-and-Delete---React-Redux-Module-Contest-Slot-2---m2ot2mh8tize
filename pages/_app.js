2b518589ae602a1672c22fa6c7c2ba6bd96a9852
manoj nayak <115462742+manoj3599@users.noreply.github.com>
2023-12-17 17:22:58 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,37 @@
 import "@/styles/globals.css";
 import React, { useEffect, useState } from "react";
+import Dashboard from "./Dashboard";
+
 export const AuthContext = React.createContext();
 
 export default function App({ Component, pageProps }) {
   const [data, setData] = useState([]);
-  const [msg, setMsg] = useState("")
+  const [msg, setMsg] = useState("");
 
-  const API = "https://content.newtonschool.co/v1/pr/63b70222af4f30335b4b3b9a/buses"
+  const API = "https://content.newtonschool.co/v1/pr/63b70222af4f30335b4b3b9a/buses";
 
+  useEffect(() => {
+    // Fetch data when the component mounts
+    fetch(API)
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+
+  const deleteRow = (id) => {
+    // Filter out the row with the specified id
+    const newData = data.filter((item) => item.id !== id);
+    setData(newData);
+    setMsg("Data Deleted");
+
+    // Reset the message after 3 seconds
+    setTimeout(() => {
+      setMsg("");
+    }, 3000);
+  };
 
   const contextValue = {
-
+    data,
+    deleteRow,
   };
 
   return (
@@ -19,3 +40,4 @@ export default function App({ Component, pageProps }) {
     </AuthContext.Provider>
   );
 }
+
